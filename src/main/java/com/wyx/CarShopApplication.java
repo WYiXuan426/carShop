4de4bd8c8d79c71998476e77067efad97e10d1b7
WYiXuan426 <145527841+WYiXuan426@users.noreply.github.com>
2023-09-21 21:35:32 +0800

    修改名称

--- src/main/java/com/wyx/CarShopApplication.java
+++ src/main/java/com/wyx/CarShopApplication.java
@@ -1,9 +1,14 @@
-package com.example.carshop;
+package com.wyx;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.wyx.mapper")
+@EnableTransactionManagement
 public class CarShopApplication {
 
     public static void main(String[] args) {
